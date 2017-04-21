60440e3dd6cd59cc87f1fa2b7f02dd7d3e3797ab
XFBY <zxuanlei@163.com>
2017-04-21 11:21:13 +0800

    tf_reg

--- tensor_flow/tf_test.py
+++ tensor_flow/tf_test.py
@@ -33,7 +33,27 @@ w = tf.Variable(tf.random_uniform([1],-1.0,1.0),name='w')
 b = tf.Variable(tf.zeros([1]),name='b')
 pre_y = w*x_data+b
 #print(w,b)
-#print(pre_y)
+print(pre_y)
+loss = tf.reduce_mean(tf.square(pre_y-y_data),name='loss')
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss,name='train')
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+#print init w&b
+print('w = ',sess.run(w),'b = ',sess.run(b),'loss = ',sess.run(loss))
+#train 20 times
+for step in range(50):
+    sess.run(train)
+    print('w = ', sess.run(w), 'b = ', sess.run(b),'loss = ',sess.run(loss))
+    pass
+writer = tf.train.SummaryWriter('./tem',sess.graph)
+pass
+plt.scatter(x_data,y_data,c='r')
+plt.plot(x_data,sess.run(w)*x_data+sess.run(b))
+plt.ylabel('y_data')
+plt.xlabel('x_data')
+plt.show()
 
 
 
